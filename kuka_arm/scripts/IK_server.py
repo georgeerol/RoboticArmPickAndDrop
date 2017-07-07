f69438522be44aa20496254dd9f8c820490b65bd
George Fouche <fouliex@gmail.com>
2017-07-07 10:20:20 -0700

    Add dh_transform_matrix and correction_matrix functions

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -19,6 +19,27 @@ from mpmath import *
 from sympy import *
 
 
+def dh_transform_matrix(alpha, a, q, d):
+    return Matrix([[cos(q), -sin(q), 0, a],
+                   [sin(q) * cos(alpha), cos(q) * cos(a), -sin(alpha), -sin(q) * d],
+                   [sin(q) * sin(alpha), cos(q) * sin(a), cos(alpha), cos(q) * d],
+                   [0, 0, 0, 1]])
+
+
+def correction_matrix():
+    # 90 degrees on the z-axis
+    r_z = Matrix([[cos(pi), -sin(pi), 0, 0],
+                  [sin(pi), cos(pi), 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+    # 90 degrees on the y-axis
+    r_y = Matrix([[cos(-pi / 2), 0, sin(-pi / 2), 0],
+                  [0, 1, 0, 0],
+                  [-sin(-pi / 2), 0, cos(-pi / 2), 0],
+                  [0, 0, 0, 1]])
+    return simplify(r_z * r_y)
+
+
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
     if len(req.poses) < 1:
@@ -34,15 +55,15 @@ def handle_calculate_IK(req):
             # Define DH param symbols
 
 
-            
+
             # Joint angle symbols
 
 
-      
+
             # Modified DH params
 
 
-            
+
             # Define Modified DH Transformation matrix
 
 
@@ -50,27 +71,27 @@ def handle_calculate_IK(req):
             # Create individual transformation matrices
 
 
-            
+
             # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+            # px,py,pz = end-effector position
+            # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
 
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                    req.poses[x].orientation.z, req.poses[x].orientation.w])
-     
+                 req.poses[x].orientation.z, req.poses[x].orientation.w])
+
             # Calculate joint angles using Geometric IK method
 
-		
+
 
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+            joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+            joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
@@ -82,6 +103,7 @@ def IK_server():
     s = rospy.Service('calculate_ik', CalculateIK, handle_calculate_IK)
     print "Ready to receive an IK request"
     rospy.spin()
+
 
 if __name__ == "__main__":
     IK_server()
